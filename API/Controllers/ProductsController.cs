f294fe95fdecd7c3b3c7a8e0471d9b857c4576a0
Matheus Oliveira <matheus_mao@hotmail.com.br>
2023-01-16 18:51:23 -0300

    Adicionar arquivos de projeto.

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,31 @@
+﻿using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _context;
+
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            return Ok(await _context.Products.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+    }
+}
